0b390596aad492cd60914bb99d5187d7ac187c9d
Devkhuman <devjkhuman17@gmail.com>
2024-12-29 16:18:11 +0000

    gitaction-code-modification2

--- testing.py
+++ testing.py
@@ -1,3 +1,6 @@
+# Calling test function from hooktest.py
+from hooktest import test
+
 """
 This module contains examples of using GitHub for collaboration.
 """
@@ -6,6 +9,4 @@ print("Hi, this is a GitHub example.")
 print("I made the first change locally to push.")
 print("I made the second change through GitHub to pull.")
 
-# Calling test function from hooktest.py
-from hooktest import test
 test()
